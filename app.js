eafa0006bd96cf05cd5dc2d450f5d814ed5f4d99
Ksenia Busquet <ksenia.busquet@gmail.com>
2022-07-09 17:50:49 -0300

    Lab solved

--- app.js
+++ app.js
@@ -2,11 +2,61 @@ const express = require("express");
 
 const PORT = 4000;
 
-// Importing all the pokemon for our data file
 const allPokemon = require("./data");
 
 const app = express();
+app.use(express.json())
+
 
 // -- Define your route listeners here! --
+
+app.get('/pokemon', (req, res) => {
+    console.log(req)
+    res.send(allPokemon)
+})
+
+app.get('/pokemon/:id', (req, res) => {
+    const { id } = req.params
+    res.send(allPokemon[id])
+})
+
+
+app.get('/search', (req, res) => {
+    const pokemonName = req.query["name"]
+    const pokemonType = req.query["type"]
+
+    const foundPokemon = allPokemon.find(pokemon => pokemon.name === pokemonName)
+    let pokemonArray = []
+
+    allPokemon.map(pokemon => {
+        if (pokemon.types.includes(pokemonType)) {
+            pokemonArray.push(poke)
+            return pokemonArray
+        }
+    })
+
+    if (pokemonName === undefined) {
+        res.send(pokemonArray)
+    } else if (pokemonType === undefined) {
+        res.send(foundPokemon)
+    }
+})
+
+app.post('/pokemon', (req, res) => {
+    let body = req.body
+    allPokemon.push(body)
+    res.send(body)
+})
+
+app.put('/pokemon/:id', (req, res) => {
+    const { id } = req.params
+    let body = req.body
+    res.send(allPokemon[id] = body)
+})
+
+app.delete('/pokemon/:id', (req, res) => {
+    const { id } = req.params
+    res.send(allPokemon[id])
+})
 
 app.listen(PORT, () => console.log(`Server up and running at port ${PORT}`));
